cc6a761c87ba9e23bdb37be851a533e59aa258f5
Shubham Raj <shubham14p3@gmail.com>
2021-04-05 01:41:40 +0530

    adding sample 1

---
+++ sample/sample1/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <>
+  <h1>React Basic</h1>
+  <p>List of 5 IMportant file of React</p>
+  <ol>
+    <li>JSX</li>
+    <li>Fragment</li>     
+    <li>Component</li>     
+    <li>ReactDOM</li>     
+    <li>HOC</li>     
+
+  </ol>
+  </>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
