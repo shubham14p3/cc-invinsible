d02aeac16f5befc607f02089bac9e05ccf68f9ec
Shubham Raj <shubham14p3@gmail.com>
2021-04-10 02:19:50 +0530

    optimised the root files

--- sample/sample2/src/index.js
+++ sample/sample2/src/index.js
@@ -1,26 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-// import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-const fname = "Shubham";
-const lname = "Raj";
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
-ReactDOM.render(
-  <>
-    <h1>My name is {fname + lname} with + number but no space </h1>
-    <h1>My name is {fname} {lname} concatenate method</h1>
-    <h1>My name is {fname+" "+lname}</h1>
-    <h1>{`My name is ${fname} ${lname} with tempelate literals`}</h1>
-    <p>My Current date is {currDate}</p>   
-    <p>My lucky number is {currTime }</p>   
-  </>,
-  document.getElementById("root")
+import React from 'react';
+import ReactDom from 'react-dom';
+const name='Shubham';
+ReactDom.render(
+  <h1>My name is {name}</h1>
+  ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
