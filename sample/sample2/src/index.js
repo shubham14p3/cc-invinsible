b91b010570a411cad503878eff635bfde1714a83
Shubham Raj <shubham14p3@gmail.com>
2021-04-11 02:23:18 +0530

    using global contentEditable JSX attribute

--- sample/sample2/src/index.js
+++ sample/sample2/src/index.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-const name='Shubham';
-ReactDom.render(
-  <h1>My name is {name}</h1>
-  ,document.getElementById('root')
-);
+import React from "react";
+import ReactDom from "react-dom";
+const name = "Shubham";
+ReactDom.render(<h1 contentEditable="true">My name is {name}</h1>, document.getElementById("root"));
