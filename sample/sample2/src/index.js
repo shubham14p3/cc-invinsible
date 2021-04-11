389b1df8f183268d75b59a2bd72a621433e7a882
Shubham Raj <shubham14p3@gmail.com>
2021-04-12 02:33:53 +0530

    using images with its dynamic form

--- sample/sample2/src/index.js
+++ sample/sample2/src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 const name = "Shubham";
-ReactDom.render(<h1 contentEditable="true">My name is {name}</h1>, document.getElementById("root"));
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+ReactDom.render(
+  <>
+<h1 contentEditable="true">My name is {name}</h1>
+<img src={img1} alt="RandomImage"/>
+<img src={img2} alt="RandomImage"/>
+<img src={img3} alt="RandomImage"/>
+</>
+, document.getElementById("root"));
