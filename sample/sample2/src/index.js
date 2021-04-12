fecc5b92b22b19cab9a6a8c552a590118ca75b7e
Shubham Raj <shubham14p3@gmail.com>
2021-04-13 02:37:46 +0530

    using images as anchor in advance form

--- sample/sample2/src/index.js
+++ sample/sample2/src/index.js
@@ -4,11 +4,15 @@ const name = "Shubham";
 const img1 = "https://picsum.photos/200/300";
 const img2 = "https://picsum.photos/250/300";
 const img3 = "https://picsum.photos/300/300";
+const link = "https://www.shubhamraj.dev";
 ReactDom.render(
   <>
 <h1 contentEditable="true">My name is {name}</h1>
 <img src={img1} alt="RandomImage"/>
 <img src={img2} alt="RandomImage"/>
+<a href={link} target="_shubham">
+  
 <img src={img3} alt="RandomImage"/>
+</a>
 </>
 , document.getElementById("root"));
