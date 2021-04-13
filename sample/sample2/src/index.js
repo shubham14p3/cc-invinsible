ff9d6d5be26f1caa9b7232ef0cf7450e4076b461
Shubham Raj <shubham14p3@gmail.com>
2021-04-14 02:46:04 +0530

    matching with scss

--- sample/sample2/src/index.js
+++ sample/sample2/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
+import './index.css';
 const name = "Shubham";
 const img1 = "https://picsum.photos/200/300";
 const img2 = "https://picsum.photos/250/300";
@@ -7,7 +8,7 @@ const img3 = "https://picsum.photos/300/300";
 const link = "https://www.shubhamraj.dev";
 ReactDom.render(
   <>
-<h1 contentEditable="true">My name is {name}</h1>
+<h1 className="heading">My name is {name}</h1>
 <img src={img1} alt="RandomImage"/>
 <img src={img2} alt="RandomImage"/>
 <a href={link} target="_shubham">
