1b5b6a0dc8e012edb227486d11d41bd474f0e128
Shubham Raj <shubham14p3@gmail.com>
2021-04-15 02:54:45 +0530

    adding images & its styling

--- sample/sample2/src/index.js
+++ sample/sample2/src/index.js
@@ -9,11 +9,13 @@ const link = "https://www.shubhamraj.dev";
 ReactDom.render(
   <>
 <h1 className="heading">My name is {name}</h1>
+<div className="container">
 <img src={img1} alt="RandomImage"/>
 <img src={img2} alt="RandomImage"/>
 <a href={link} target="_shubham">
   
 <img src={img3} alt="RandomImage"/>
 </a>
+</div>
 </>
 , document.getElementById("root"));
