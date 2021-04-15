891d7c6759a2c16eb730d3e5556bde37a92af692
Shubham Raj <shubham14p3@gmail.com>
2021-04-16 03:32:42 +0530

    creating first logics

--- sample/sample2/src/index.js
+++ sample/sample2/src/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDom from "react-dom";
 import './index.css';
-const name = "Shubham";
-const img1 = "https://picsum.photos/200/300";
-const img2 = "https://picsum.photos/250/300";
-const img3 = "https://picsum.photos/300/300";
-const link = "https://www.shubhamraj.dev";
+let curDate = new Date();
+curDate =curDate.getHours();
+let greeting="";
+const cssStyle={};
+
+if (curDate>=1 &&curDate<12){
+  greeting ="Good Morning";
+  cssStyle.color="green";
+}else if(curDate>=12 && curDate<19){
+}else{
+  greeting ="Good Evening";
+  cssStyle.color="Yellow";  
+}
+
 ReactDom.render(
   <>
-<h1 className="heading">My name is {name}</h1>
-<div className="container">
-<img src={img1} alt="RandomImage"/>
-<img src={img2} alt="RandomImage"/>
-<a href={link} target="_shubham">
-  
-<img src={img3} alt="RandomImage"/>
-</a>
-</div>
+<p>Hello now is {curDate} and <span style={cssStyle}>{greeting}</span></p>
 </>
 , document.getElementById("root"));
