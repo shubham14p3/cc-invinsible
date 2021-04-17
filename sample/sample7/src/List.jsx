c45410af6a6db56d8393c29e8479b50e5c33f44d
Shubham Raj <shubham14p3@gmail.com>
2021-04-18 04:33:01 +0530

    defined Calculator

--- sample/sample7/src/List.jsx
+++ sample/sample7/src/List.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import * as Calcu from "./Calculator";
 function List(){
 return (
 <ol>
-<li>ReactDOM</li>
-<li>JSX</li>
-<li>HOC</li>
-<li>Component</li>
-<li>JSX Component</li>
+<li>sum of two no us {Calcu.add(40,3)}</li>
+<li>sum of two no us {Calcu.sub(40,3)}</li>
+<li>sum of two no us {Calcu.div(40,3)}</li>
+<li>sum of two no us {Calcu.mul(40,3)}</li>
 </ol>);
 }
 export default List;
