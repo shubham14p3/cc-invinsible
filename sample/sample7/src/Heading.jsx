0ed6c1c2be3e6e7b98c55e22d6995b5b3898fd3b
Shubham Raj <shubham14p3@gmail.com>
2021-04-20 03:53:57 +0530

    adding first components

---
+++ sample/sample7/src/Heading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Heading(){
+return <h1>React Basic</h1>;
+}
+export default Heading;
--- sample/sample7/src/index.js
+++ sample/sample7/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Heading from "./Heading";
 
 ReactDOM.render(
-  <></>,
+  <>
+    <Heading/>
+  </>,
   document.getElementById('root')
 );
 
