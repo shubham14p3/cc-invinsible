6c1006b2d55bf2abe412b1d255b1ca9afc2a9947
Shubham Raj <shubham14p3@gmail.com>
2021-04-21 03:55:42 +0530

    adding first second component

---
+++ sample/sample7/src/Para.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Para(){
+return <p>React has the following</p>;
+}
+export default Para;
--- sample/sample7/src/index.js
+++ sample/sample7/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Heading from "./Heading";
+import Para from "./Para";
 
 ReactDOM.render(
   <>
     <Heading/>
+    <Para/>
   </>,
   document.getElementById('root')
 );
