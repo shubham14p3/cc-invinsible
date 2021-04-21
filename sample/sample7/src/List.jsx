63e5f7d08b33f4ef0dde08d0f33084d4b7577511
Shubham Raj <shubham14p3@gmail.com>
2021-04-22 03:58:20 +0530

    adding list component

---
+++ sample/sample7/src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function List(){
+return (
+<ol>
+<li>ReactDOM</li>
+<li>JSX</li>
+<li>HOC</li>
+<li>Component</li>
+<li>JSX Component</li>
+</ol>);
+}
+export default List;
--- sample/sample7/src/index.js
+++ sample/sample7/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Heading from "./Heading";
 import Para from "./Para";
+import List from "./List";
 
 ReactDOM.render(
   <>
     <Heading/>
     <Para/>
+    <List/>
   </>,
   document.getElementById('root')
 );
