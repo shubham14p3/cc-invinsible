84679e18c3482cf1c3d85529c3be845055b43121
Shubham Raj <shubham14p3@gmail.com>
2021-04-24 04:36:24 +0530

    All functions added and made working

--- sample/sample7/src/Calculator.jsx
+++ sample/sample7/src/Calculator.jsx
@@ -1,19 +1,19 @@
 // import React from "react";
 // function Calculator(){
 
-    function add(a,b){
-        return a+b;
-    }
-    function sub(a,b){
-        return a-b;
-    };
-    function div(a,b){
-        return a/b;
-    };
-    function mul(a,b){
-        return a*b;
-    };
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function div(a, b) {
+  let div = a / b;
+  div = div.toFixed(2);
+  return div;
+} 
+function mul(a, b) {
+  return a * b;
+}
 
-
-
-export {add,sub,div,mul};
+export { add, sub, div, mul };
