246d8aa55bcad5515d5b9c1055613a7498f06562
Shubham Raj <shubham14p3@gmail.com>
2021-04-26 04:49:48 +0530

    Starting folder structure

--- sample/sample8/src/App.js
+++ sample/sample8/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="cards">
+        <div className="card">
+          <img src="" alt="myPic" className="card__img"/>
+          <div className="card__info">
+            <span className="card__category"></span>
+            <h3 className="card__title"></h3>
+            <a href="" target="_blank">
+              <button>Watch Now</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
