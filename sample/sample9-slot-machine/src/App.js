229fa5cfbbdf958ca5c67a1ba84e9d4cb4825503
Shubham Raj <shubham14p3@gmail.com>
2021-05-02 14:31:59 +0530

    Adding all functions for Slot Machine

--- sample/sample9-slot-machine/src/App.js
+++ sample/sample9-slot-machine/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SlotM from "./SlotMach";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading_style"> Welcome to <span style={{fontWeight:"bold"}}>Slot Machine </span> </h1>
+      <div>
+      <SlotM x='A' y='A' z='B'/>
+      <SlotM x='A' y='A' z='B'/>
+      <SlotM x='A' y='A' z='B'/>
+       </div>      
+    </>
   );
 }
 
