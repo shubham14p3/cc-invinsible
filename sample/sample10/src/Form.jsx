7692dfc76e70decee232bbd0414a1b888b754e45
Shubham Raj <shubham14p3@gmail.com>
2021-05-06 17:38:55 +0530

    Using Single Compoent complex Form

--- sample/sample10/src/Form.jsx
+++ sample/sample10/src/Form.jsx
@@ -1,44 +1,58 @@
 import React, { useState } from "react";
 
 const Form = () => {
-  const [name, setName] = useState();
-  const [fullName, setFullName] = useState();
-  const [lastName, setLastName] = useState();
-  const [lastNameNew, setLastNameNew] = useState();
+  const [fullName, setFullName] = useState({
+      fname: "",
+      lname:"",
+  });
 
   const onSubmit = (event) => {
     event.preventDefault();
-    setFullName(name);
-    setLastNameNew(lastName);
+    alert('Form Submitted');
+
   };
 
   const inputEvent = (event) => {
     // console.log("clicked");
-    setName(event.target.value);
-  };
+    // console.log(event.target.value);
+    const value=event.target.value;
+    const name=event.target.name;
+    setFullName((preValue)=>{
+        if (name==='fname'){
+            return{
+                fname: value,
+                lname:preValue.lname,
+            };
+        } else if(name==='lname') {
+            return{
+                fname: preValue.fname,
+                lname:value,
+            };
+        }
+    });
 
-  const inputEventTwo = (event) => {
-    // console.log("clicked");
-    setLastName(event.target.value);
-  };
+      };
+
   return (
     <>
       <div className="main_div">
         <form onClick={onSubmit}>
           <div>
-            <h1>Hello ! {fullName}  {lastNameNew}</h1>
+            <h1>Hello ! {fullName.fname}  {fullName.lname}</h1>
             <input
               type="text"
               placeholder="Enter your First Name"
+              name='fname'
               onChange={inputEvent}
-              value={name}
+              value={fullName.fname}
             ></input>
 
             <input
               type="text"
               placeholder="Enter your Second Name"
-              onChange={inputEventTwo}
-              value={lastName}
+              name='lname'
+              onChange={inputEvent}
+              value={fullName.lname}
             ></input>
             <button type="submit">Click Here 👍</button>
           </div>
