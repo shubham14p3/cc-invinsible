6397b4c8e95169c3e2cc7e6c2c42d611d7d3ebd6
Shubham Raj <shubham14p3@gmail.com>
2021-05-06 15:31:01 +0530

    added hooks for count for Basic Click Me

--- sample/sample10/src/App.js
+++ sample/sample10/src/App.js
@@ -1,10 +1,20 @@
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount]= useState(1); //array destructuring
+
+const IncNum=()=>{
+  // console.log(IncNum);
+  setCount(count+1);
+  console.log("Clicked");
+  // console.log(count);
+}
+
   return (
     <>
-    <h1>0</h1>
-    <button>Click Me</button>
+    <h1>{count}</h1>
+    <button onClick={IncNum}>Click Me</button>
     </>
   );
 }
