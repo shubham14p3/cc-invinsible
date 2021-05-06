2f5b0b1fac2ce20b5d72a6fa8ab30c332befb83d
Shubham Raj <shubham14p3@gmail.com>
2021-05-06 19:36:52 +0530

    Complete To-Do List working app basic

--- sample/sample11/src/App.js
+++ sample/sample11/src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
+import ToDoLists from "./ToDoLists";
 function App() {
+  const [inputList, setInputList] = useState("by apple");
+
+  const [Items, setItems] = useState([]);
+
+  const itemEvents = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItem = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+  const deleteItems = (id) => {
+    // console.log('deleted');
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add a Items"
+            value={inputList}
+            onChange={itemEvents}
+          />
+          <button onClick={listOfItem}>+</button>
+          <ol>
+            {Items.map((itemval, index) => {
+              return (
+                <ToDoLists
+                  key={index}
+                  text={itemval}
+                  id={index}
+                  onSelect={deleteItems}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
