9cd4147477c62c16032aea52cccbe8b27a7023e8
Shubham Raj <shubham14p3@gmail.com>
2021-05-06 18:13:22 +0530

    Refactored Codes

--- sample/sample10/src/Form.jsx
+++ sample/sample10/src/Form.jsx
@@ -21,35 +21,40 @@ const Form = () => {
     // const name=event.target.name;
     const {value,name}=event.target;
     setFullName((preValue)=>{
-        if (name==='fname'){
-            return{
-                fname: value,
-                lname:preValue.lname,
-                email:preValue.email,
-                phone:preValue.phone,
-            };
-        } else if(name==='lname') {
-            return{
-                fname: preValue.fname,
-                lname:value,
-                email:preValue.email,
-                phone:preValue.phone,
-            };
-        } else if(name==='email') {
-            return{
-                fname: preValue.fname,
-                lname:preValue.lname,
-                email:value,
-                phone:preValue.phone,
-            };
-        } else if(name==='phone') {
-            return{
-                fname: preValue.fname,
-                lname:preValue.lname,
-                email:preValue.email,
-                phone:value,
-            };
+
+        return{
+            ...preValue,
+            [name]:value,
         }
+        // if (name==='fname'){
+        //     return{
+        //         fname: value,
+        //         lname:preValue.lname,
+        //         email:preValue.email,
+        //         phone:preValue.phone,
+        //     };
+        // } else if(name==='lname') {
+        //     return{
+        //         fname: preValue.fname,
+        //         lname:value,
+        //         email:preValue.email,
+        //         phone:preValue.phone,
+        //     };
+        // } else if(name==='email') {
+        //     return{
+        //         fname: preValue.fname,
+        //         lname:preValue.lname,
+        //         email:value,
+        //         phone:preValue.phone,
+        //     };
+        // } else if(name==='phone') {
+        //     return{
+        //         fname: preValue.fname,
+        //         lname:preValue.lname,
+        //         email:preValue.email,
+        //         phone:value,
+        //     };
+        // }
     });
 
       };
