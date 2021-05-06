c86af6d0ef11682f6f72a3523c9869d5d7449bee
Shubham Raj <shubham14p3@gmail.com>
2021-05-06 17:50:00 +0530

    Adding Name , LName, Email, Phone to form

--- sample/sample10/src/Form.jsx
+++ sample/sample10/src/Form.jsx
@@ -4,29 +4,50 @@ const Form = () => {
   const [fullName, setFullName] = useState({
       fname: "",
       lname:"",
+      email:"",
+      phone:""
   });
 
   const onSubmit = (event) => {
     event.preventDefault();
-    alert('Form Submitted');
+    // alert('Form Submitted');
 
   };
 
   const inputEvent = (event) => {
     // console.log("clicked");
     // console.log(event.target.value);
-    const value=event.target.value;
-    const name=event.target.name;
+    // const value=event.target.value;
+    // const name=event.target.name;
+    const {value,name}=event.target;
     setFullName((preValue)=>{
         if (name==='fname'){
             return{
                 fname: value,
                 lname:preValue.lname,
+                email:preValue.email,
+                phone:preValue.phone,
             };
         } else if(name==='lname') {
             return{
                 fname: preValue.fname,
                 lname:value,
+                email:preValue.email,
+                phone:preValue.phone,
+            };
+        } else if(name==='email') {
+            return{
+                fname: preValue.fname,
+                lname:preValue.lname,
+                email:value,
+                phone:preValue.phone,
+            };
+        } else if(name==='phone') {
+            return{
+                fname: preValue.fname,
+                lname:preValue.lname,
+                email:preValue.email,
+                phone:value,
             };
         }
     });
@@ -38,7 +59,7 @@ const Form = () => {
       <div className="main_div">
         <form onClick={onSubmit}>
           <div>
-            <h1>Hello ! {fullName.fname}  {fullName.lname}</h1>
+            <h1>Hello ! {fullName.fname}  {fullName.lname} {fullName.email} {fullName.phone}</h1>
             <input
               type="text"
               placeholder="Enter your First Name"
@@ -53,6 +74,20 @@ const Form = () => {
               name='lname'
               onChange={inputEvent}
               value={fullName.lname}
+            ></input>
+            <input
+              type="email"
+              placeholder="Enter your Eamil"
+              name='email'
+              onChange={inputEvent}
+              value={fullName.email}
+            ></input>
+            <input
+              type="number"
+              placeholder="Enter your Mobile Number"
+              name='phone'
+              onChange={inputEvent}
+              value={fullName.phone}
             ></input>
             <button type="submit">Click Here 👍</button>
           </div>
