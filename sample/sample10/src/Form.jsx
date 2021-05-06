9f2ec918d4c5098e31ca2e6f37689a13ec8c43f2
Shubham Raj <shubham14p3@gmail.com>
2021-05-06 17:14:38 +0530

    Using Basic Form Component

--- sample/sample10/src/Form.jsx
+++ sample/sample10/src/Form.jsx
@@ -1,29 +1,48 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [name, setName] = useState();
+  const [fullName, setFullName] = useState();
+  const [lastName, setLastName] = useState();
+  const [lastNameNew, setLastNameNew] = useState();
 
-    const [name,setName]= useState();
-    const [fullname,setFullName]= useState();
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFullName(name);
+    setLastNameNew(lastName);
+  };
+
   const inputEvent = (event) => {
-    console.log("clicked");
+    // console.log("clicked");
     setName(event.target.value);
   };
 
-  const onSubmit=()=>{
-    setFullName(name);
-  }
-
+  const inputEventTwo = (event) => {
+    // console.log("clicked");
+    setLastName(event.target.value);
+  };
   return (
     <>
-      <div>
-        <h1>Hello ! {fullname}</h1>
-        <input
-          type="text"
-          placeholder="Enter your Name"
-          onChange={inputEvent}
-          value={name}
-        ></input>
-        <button onClick={onSubmit}>Click Here 👍</button>
+      <div className="main_div">
+        <form onClick={onSubmit}>
+          <div>
+            <h1>Hello ! {fullName}  {lastNameNew}</h1>
+            <input
+              type="text"
+              placeholder="Enter your First Name"
+              onChange={inputEvent}
+              value={name}
+            ></input>
+
+            <input
+              type="text"
+              placeholder="Enter your Second Name"
+              onChange={inputEventTwo}
+              value={lastName}
+            ></input>
+            <button type="submit">Click Here 👍</button>
+          </div>
+        </form>
       </div>
     </>
   );
